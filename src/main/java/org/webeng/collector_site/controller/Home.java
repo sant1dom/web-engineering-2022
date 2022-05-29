8e9c7d17e3ff51c040e29f5b82cd57d377c0355f
Davide <davide.deacetis13@gmail.com>
2022-05-29 11:09:55 +0200

    aggiunte viste di login e templating commit 1

---
+++ src/main/java/org/webeng/collector_site/controller/Home.java
@@ -0,0 +1,88 @@
+/*
+ * Login.java
+ *
+ * Questo esempio mostra come utilizzare le sessioni per autenticare un utente
+ * 
+ * This example shows how to use sessions to authenticate the user
+ *
+ */
+package org.webeng.collector_site.controller;
+
+import org.webeng.collector_site.data.dao.CollectorsDataLayer;
+import org.webeng.collector_site.data.model.Utente;
+import org.webeng.framework.data.DataException;
+import org.webeng.framework.result.TemplateManagerException;
+import org.webeng.framework.result.TemplateResult;
+import org.webeng.framework.security.SecurityHelpers;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Home extends CollectorsBaseController {
+
+    public static final String REFERRER = "referrer";
+
+    private void action_default(HttpServletRequest request, HttpServletResponse response) throws TemplateManagerException {
+        TemplateResult result = new TemplateResult(getServletContext());
+        request.setAttribute(REFERRER, request.getParameter(REFERRER));
+        result.activate("index.ftl", request, response);
+    }
+
+    private void action_login(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("u");
+        String password = SecurityHelpers.encryptPassword(request.getParameter("p"));
+        //... VALIDAZIONE IDENTITA'...
+        //... IDENTITY CHECKS ...
+        Utente utente = null;
+        try {
+            utente = ((CollectorsDataLayer) request.getAttribute("datalayer")).getUtenteDAO().doLogin(username, password);
+        } catch (DataException ex) {
+            handleError(ex, request, response);
+        }
+
+        if (utente != null) {
+            //se la validazione ha successo
+            //if the identity validation succeeds
+            //carichiamo lo userid dal database utenti
+            //load userid from user database
+
+            int userid = utente.getKey();
+            SecurityHelpers.createSession(request, username, userid);
+            //se è stato trasmesso un URL di origine, torniamo a quell'indirizzo
+            //if an origin URL has been transmitted, return to it
+            if (request.getParameter(REFERRER) != null) {
+                response.sendRedirect(request.getParameter(REFERRER));
+            } else {
+                response.sendRedirect("home");
+            }
+        } else {
+            handleError("Login failed", request, response);
+        }
+    }
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     */
+    @Override
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException {
+        try {
+            if (request.getParameter("login") != null) {
+                action_login(request, response);
+            } else {
+                String https_redirect_url = SecurityHelpers.checkHttps(request);
+                request.setAttribute("https-redirect", https_redirect_url);
+                action_default(request, response);
+            }
+        } catch (IOException | TemplateManagerException ex) {
+            handleError(ex, request, response);
+        }
+    }
+}
--- src/main/webapp/templates/outline.ftl
+++ src/main/webapp/templates/outline.ftl
@@ -1,31 +1,10 @@
 <?xml version="1.0" encoding="ISO-8859-1"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <title>My Newspaper</title>
-        <link rel="stylesheet" href="style/normalize.css" type="text/css"/>        
-        <link rel="stylesheet" href="style/grid.css" type="text/css"/>
-        <link rel="stylesheet" href="style/default.css" type="text/css"/>        
-    </head>
-    <body>
-        <div class="container">
-            <#include "outline_header.ftl">
-            <div class="row" id="body">
-
-                <div class="three columns">
-                    <div class="vertical menu">
-                    <ul>
-                        <li><a href="issues">newspaper homepage</a></li>
-                        <li><a href="compose">compose issue</a></li>
-                        <li><a href="write">write article</a></li>
-                    </ul>
-                        </div>
-                </div>
-                <div class="thirteen columns" >
-                    <#include content_tpl>
-                </div>
-            </div>
-            <#include "outline_footer.ftl">
-        </div>
-    </body>
+<#include "outline_head.ftl">
+<body class="app sidebar-mini ltr light-mode">
+<#include "outline_header.ftl">
+<#include content_tpl>
+<#include "outline_footer.ftl">
+</body>
 </html>
