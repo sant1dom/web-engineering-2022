d1604e37ed941b4edbfecb492b86290c1aa951f5
Davide <davide.deacetis13@gmail.com>
2022-07-03 10:59:30 +0200

    Fix stats e rimozione eccezioni freemarker

--- src/main/webapp/assets/js/stats.js
+++ src/main/webapp/assets/js/stats.js
@@ -1,69 +1,78 @@
 $(document).ready(function () {
-    $('#autore').selectpicker();
-    $('#genere').selectpicker();
-    $('#etichetta').selectpicker();
+    const autore = $('#autore');
+    const genere = $('#genere');
+    const etichetta = $('#etichetta')
+    const anno = $('#anno');
 
-    $('#autore').change(function () {
+    autore.selectpicker();
+    genere.selectpicker();
+    anno.selectpicker();
+
+    autore.change(function () {
+        let nda = $("#nda");
         if ($(this).val() !== "") {
             let autore = $("#autore option:selected").text();
             $.ajax({
-                url: 'http://localhost:8080/stats?action=autore&autore=' + autore,
+                url: 'stats?action=autore&autore=' + autore,
                 type: 'GET',
                 success: function (data) {
-                    $("#nda").text("Numero di dischi per l'autore selezionato: " + data);
-                    $("#nda").css("display", "");
+                    nda.text("Numero di dischi per l'autore selezionato: " + data);
+                    nda.css("display", "");
                 }
             });
         } else {
-            $("#nda").css("display", "none");
+            nda.css("display", "none");
         }
     });
 
-    $('#genere').change(function () {
+    genere.change(function () {
+        let nda = $("#ndg");
         if ($(this).val() !== "") {
             let genere = $("#genere option:selected").text();
             $.ajax({
-                url: 'http://localhost:8080/stats?action=genere&genere=' + genere,
+                url: 'stats?action=genere&genere=' + genere,
                 type: 'GET',
                 success: function (data) {
-                    $("#ndg").text("Numero di dischi per il genere selezionato: " + data);
-                    $("#ndg").css("display", "");
+                    nda.text("Numero di dischi per il genere selezionato: " + data);
+                    nda.css("display", "");
                 }
             });
         } else {
-            $("#ndg").css("display", "none");
+            nda.css("display", "none");
         }
     });
 
-    $('#etichetta').change(function () {
+    etichetta.change(function () {
+        let nde = $("#nde");
         if ($(this).val() !== "") {
             let etichetta = $("#etichetta option:selected").text();
             $.ajax({
-                url: 'http://localhost:8080/stats?action=etichetta&etichetta=' + etichetta,
+                url: 'stats?action=etichetta&etichetta=' + etichetta,
                 type: 'GET',
                 success: function (data) {
-                    $("#nde").text("Numero di dischi per l'etichetta selezionata: " + data);
-                    $("#nde").css("display", "");
+                    nde.text("Numero di dischi per l'etichetta selezionata: " + data);
+                    nde.css("display", "");
                 }
             });
         } else {
-            $("#nde").css("display", "none");
+            nde.css("display", "none");
         }
     });
 
-    $('#anno').change(function () {
+    anno.change(function () {
+        let ndanno = $("#ndanno");
         if ($(this).val() !== "") {
             let anno = $("#anno").val();
             $.ajax({
-                url: 'http://localhost:8080/stats?action=anno&anno=' + anno,
+                url: 'stats?action=anno&anno=' + anno,
                 type: 'GET',
                 success: function (data) {
-                    $("#ndanno").text("Numero di dischi per l'anno selezionato: " + data);
-                    $("#ndanno").css("display", "");
+                    ndanno.text("Numero di dischi per l'anno selezionato: " + data);
+                    ndanno.css("display", "");
                 }
             });
         } else {
-            $("#ndanno").css("display", "none");
+            ndanno.css("display", "none");
         }
     });
 });
